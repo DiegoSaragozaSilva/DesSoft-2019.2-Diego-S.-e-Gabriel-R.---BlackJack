fd8909b79890602cbae97ffe2c3c9a5ef9df52a2
Gabriel Ribeiro <gabriel@omaximo.com.br>
2019-09-30 16:46:36 -0300

    adiconar mais variaveis para derrota ou vitoria + entrega de mais cartas

--- main.py
+++ main.py
@@ -28,32 +28,32 @@ random.shuffle(Baralho)
 
 #Função que retorna o valor de duas cartas
 def retornaCartas():
-    a = [0] * 2
-    for i in range(2):
+    a = [0] * 1
+    for i in range(1):
         x = random.randint(0, len(Baralho) - 1)
         a[i] = Baralho.pop(x)
     return a
 
 #Loop principal
-while Jogando:
+while Jogando and Dinheiro>0:
     #Recebimento da aposta
     print("Seu dinheiro é U${0}".format(Dinheiro))
     aposta = int(input("Digite o quanto vai apostar:"))
 
 
     #Verificação de aposta inválida
-    if aposta < 1:
+    if aposta < 1 or aposta>Dinheiro:
         print("Aposta invalida! Aposte valores maiores que 0!")
         aposta = int(input("Digite o quanto vai apostar:"))
 
-    else:
+    elif aposta>=1:
         while Apostando:
             #Loop de apostas
             Dinheiro -= aposta
             cartasJ = retornaCartas()
-            SomaJ += cartasJ[0] + cartasJ[1]
+            SomaJ += cartasJ[0] + cartasJ[0]
             cartasPC = retornaCartas()
-            SomaPC += cartasPC[0] + cartasPC[1]
+            SomaPC += cartasPC[0] + cartasPC[0]
 
             #Verificação de Blackjack
             if SomaJ == 21:
@@ -61,16 +61,17 @@ while Jogando:
                 Dinheiro += Dinheiro * 1.5
             
             #Verificação de continuidade da aposta
-            elif SomaJ < 21:
+            while SomaJ < 21:
                 print("Você tem {0} pontos.".format(SomaJ))
-                resp = input("Deseja mais duas cartas? Digite 'sim' ou 'não'")
+                resp = input("Deseja mais uma cartas? Digite 'sim' ou 'não'")
                 if resp == "não":
                     Apostando = False
-            
-            #Verificação de derrota
-            elif SomaJ > 21:
-                resp = input("Você perdeu! Deseja continuar jogando? Digite 'sim' ou 'não'")
-                if resp == 'não':
-                    Jogando = False
-                    Apostando = False
-                    
+                elif resp == "sim":
+                    SomaJ=SomaJ + cartasJ[0]
+    #Verificação de derrota
+    elif SomaJ > 21 or SomaPC>SomaJ:
+        resp = input("Você perdeu! Deseja continuar jogando? Digite 'sim' ou 'não'")
+        if resp == 'não':
+            Jogando = False
+            Apostando = False
+             

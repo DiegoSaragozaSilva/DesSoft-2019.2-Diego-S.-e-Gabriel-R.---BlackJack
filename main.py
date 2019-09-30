bcc8c0850cb6831ac6c1ab3175fdfbf6cd8097c3
Diego Saragoza Da Silva <diegosaragozaa2@gmail.com>
2019-09-30 15:15:31 -0300

    Adição de controles de apostas e respostas, verificações de vitória, blackjack ou derrota. Melhoria no loop principal

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #Imports para o código
 import random
 
-#Criação do baralho como uma lista 2D
+#Criação do baralho como uma lista
 Baralho = [0] * 13
-
 #Dinheiro inicial do player
 Dinheiro=100
+#Soma das cartas na mão do jogador e da máquina
+SomaJ = 0
+SomaPC = 0
+#Variável que controla quando o jogador está jogando e apostando ou não
+Jogando = True
+Apostando = True
 
 #Atribuição de valores nas cartas do baralho
 for i in range(0, len(Baralho)):
@@ -17,25 +22,54 @@ for i in range(0, len(Baralho)):
         Baralho[i] = 10
 
 Baralho *= 4
-print(Baralho)
+
 #Randomização do baralho
 random.shuffle(Baralho)
 
+#Função que retorna o valor de duas cartas
 def retornaCartas():
+    a = [0] * 2
     for i in range(2):
         x = random.randint(0, len(Baralho) - 1)
-        print(x)
-        print(Baralho)
-        a = Baralho[x]
-        Baralho.pop(x)
+        a[i] = Baralho.pop(x)
     return a
 
-#Loop de apostas
-aposta = int(input("Digite o quato vai apostar:"))
-while aposta < 1:
-    print("aposta invalida")
-    aposta = int(input("Digite o quato vai apostar:"))
+#Loop principal
+while Jogando == True:
+    #Recebimento da aposta
+    print("Seu dinheiro é U${0}".format(Dinheiro))
+    aposta = int(input("Digite o quanto vai apostar:"))
 
-while aposta >= 1:
-    Dinheiro -= aposta
-    a = retornaCartas()
+    #Verificação de aposta inválida
+    if aposta < 1:
+        print("Aposta invalida! Aposte valores maiores que 0!")
+        aposta = int(input("Digite o quanto vai apostar:"))
+
+    else:
+        while Apostando:
+            #Loop de apostas
+            Dinheiro -= aposta
+            cartasJ = retornaCartas()
+            SomaJ += cartasJ[0] + cartasJ[1]
+            cartasPC = retornaCartas()
+            SomaPC += cartasPC[0] + cartasPC[1]
+
+            #Verificação de Blackjack
+            if SomaJ == 21:
+                print("BLACKJACK! Você ganhou!")
+                Dinheiro += Dinheiro * 1.5
+            
+            #Verificação de continuidade da aposta
+            elif SomaJ < 21:
+                print("Você tem {0} pontos.".format(SomaJ))
+                resp = input("Deseja mais duas cartas? Digite 'sim' ou 'não'")
+                if resp == "não":
+                    Apostando == False
+            
+            #Verificação de derrota
+            elif SomaJ > 21:
+                resp = input("Você perdeu! Deseja continuar jogando? Digite 'sim' ou 'não'")
+                if resp == 'não':
+                    Jogando == False
+                    Apostando == False
+                    

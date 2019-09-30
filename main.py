1b5188914599e9e6b38628cddd6d9c5f2d495825
Gabriel Ribeiro <gabriel@omaximo.com.br>
2019-09-30 11:11:37 -0300

    garantia de apostas maiores que 1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 #Criação do baralho como uma lista 2D
 Baralho = [[0] * 13] * 4
-
+#criacao da aposta inicial
+ApostaInicial=100
 
 #Atribuição de valores nas cartas do baralho
 for i in range(4):
@@ -13,3 +14,7 @@ for i in range(4):
             Baralho[i][j] = j
         else:
             Baralho[i][j] = 10
+aposta=int(input("Digite o quato vai apostar:"))
+while a<1:
+    print("aposta invalida")
+    a=int(input("Digite o quato vai apostar:"))

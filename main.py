1b23df227042790d4908b9d65d0a52c78e8893f3
Diego Saragoza Da Silva <diegosaragozaa2@gmail.com>
2019-09-30 10:57:19 -0300

    Criação do baralho e atribuição de valores as cartas

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-######TESTE######
+
+#Criação do baralho como uma lista 2D
+Baralho = [[0] * 13] * 4
+
+
+#Atribuição de valores nas cartas do baralho
+for i in range(4):
+    for j in range(13):
+        print(i, j)
+        if j == 0:
+            Baralho[i][j] = 11
+        elif j < 10:
+            Baralho[i][j] = j
+        else:
+            Baralho[i][j] = 10

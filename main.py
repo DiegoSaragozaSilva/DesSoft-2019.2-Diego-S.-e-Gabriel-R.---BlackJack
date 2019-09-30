9f55591c0c663b56d8fede35d7a4024272216b16
Diego Saragoza Da Silva <diegosaragozaa2@gmail.com>
2019-09-30 11:59:40 -0300

    Baralho em uma dimensão

--- main.py
+++ main.py
@@ -2,27 +2,40 @@
 import random
 
 #Criação do baralho como uma lista 2D
-Baralho = [[0] * 13] * 4
+Baralho = [0] * 13
 
-#criacao da aposta inicial
-ApostaInicial=100
+#Dinheiro inicial do player
+Dinheiro=100
 
 #Atribuição de valores nas cartas do baralho
-for i in range(4):
-    for j in range(13):
-        print(i, j)
-        if j == 0:
-            Baralho[i][j] = 11
-        elif j < 10:
-            Baralho[i][j] = j
-        else:
-            Baralho[i][j] = 10
+for i in range(0, len(Baralho)):
+    if i == 0:
+        Baralho[i] = 11
+    elif i < 10:
+        Baralho[i] = i + 1
+    else:
+        Baralho[i] = 10
 
+Baralho *= 4
+print(Baralho)
 #Randomização do baralho
-random.shuffle([random.shuffle(x) for x in Baralho])
+random.shuffle(Baralho)
+
+def retornaCartas():
+    for i in range(2):
+        x = random.randint(0, len(Baralho) - 1)
+        print(x)
+        print(Baralho)
+        a = Baralho[x]
+        Baralho.pop(x)
+    return a
 
 #Loop de apostas
 aposta = int(input("Digite o quato vai apostar:"))
 while aposta < 1:
     print("aposta invalida")
     aposta = int(input("Digite o quato vai apostar:"))
+
+while aposta >= 1:
+    Dinheiro -= aposta
+    a = retornaCartas()
